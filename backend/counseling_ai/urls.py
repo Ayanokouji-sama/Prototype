5d7a32c54101f3adaf80a976110fb7e5110d1ae0
Mohammad Bilal <mohdbilal4291@gmail.com>
2025-09-20 04:09:21 +0530

    bug fix

--- backend/counseling_ai/urls.py
+++ backend/counseling_ai/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')), # This line connects to your api app's URLs
+    path('api/', include('backend.api.urls')), # This line connects to your api app's URLs
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
